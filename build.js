f76a0cbfe3fa55d0fd8fe0ace7d7693ec9cdff6a
jimcal <jimcal.liu@gmail.com>
2018-12-23 11:52:22 -0800

    Add pronouns. fix twitter handle.

--- build.js
+++ build.js
@@ -18,7 +18,11 @@ const options = {
 // Text + chalk definitions
 const data = {
 	name: chalk.white.bold( "             Jim Liu" ),
-	handle: chalk.cyan( "jimcal" ),
+	handle: chalk.cyan( "@jimcalliu" ),
+	pronouns: chalk.magenta( "             Pronouns" ),
+	pronounsHe: chalk.magenta( "he" ),
+	pronounsHis: chalk.magenta( "him" ),
+	pronounsHim: chalk.magenta( "his" ),
 	work: chalk.white( "Software Engineer @ Nordstrom" ),
 	workUrl: chalk.cyan( "--==--==--==--==--==--==--" ),
 	twitter: chalk.cyan( "https://twitter.com/jimcalliu" ),
@@ -26,6 +30,7 @@ const data = {
 	linkedin: chalk.cyan( "https://linkedin.com/in/jimycliu" ),
 	volunteer: chalk.cyan( "https://2018.cascadiajs.com" ),
 	web: chalk.cyan( "https://itsj.im" ),
+	// bio: chalk.yellow( "Building & Thinking about primitives💭 Travel🛫 Coffee☕Football🏈Automobile🏎." ),
 	npx: chalk.white( "npx jimcal" ),
 	labelWork: chalk.white.bold( "      Work:" ),
 	labelWorkUrl: chalk.white.bold( "           " ),
@@ -35,12 +40,13 @@ const data = {
 	labelVolunteer: chalk.white.bold( " Volunteer:" ),
 	labelWeb: chalk.white.bold( "       Web:" ),
 	labelCard: chalk.white.bold( "      Card:" ),
-	bio: chalk.yellow( "Building & Thinking about primitives💭 Travel🛫 Coffee☕Football🏈Automobile🏎 he/him/his. http://itsj.im." )
+	labelBio: chalk.white.bold( "       Bio: " )
 };
 
 // Actual strings we're going to output
 const newline = "\n";
 const heading = `${ data.name } / ${ data.handle }`;
+const pronouns = `${ data.pronouns }:${ data.pronounsHe }/${ data.pronounsHis }/${ data.pronounsHim }`;
 const working = `${ data.labelWork }  ${ data.work }`;
 const twittering = `${ data.labelTwitter }  ${ data.twitter }`;
 const npming = `${ data.labelnpm }  ${ data.npm }`;
@@ -48,18 +54,21 @@ const githubing = `${ data.labelGitHub }  ${ data.github }`;
 const linkedining = `${ data.labelLinkedIn }  ${ data.linkedin }`;
 const volunteering = `${ data.labelVolunteer }  ${ data.volunteer }`;
 const webing = `${ data.labelWeb }  ${ data.web }`;
+// const bio = `${ data.labelBio } ${ data.bio }`;
 const carding = `${ data.labelCard }  ${ data.npx }`;
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output = banner + newline +
-               heading + // data.name + data.handle
+               heading + newline + // data.name + data.handle
+               pronouns +
                newline + newline + // Add one whole blank line
                working + newline + // data.labelWork + data.work
                twittering + newline + // data.labelTwitter + data.twitter
                githubing + newline + // data.labelGitHub + data.github
                linkedining + newline + // data.labelLinkedIn + data.linkedin
                volunteering + newline + // data.labelVolunteer + data.volunteer
-               webing + newline + newline + // data.labelWeb + data.web
+               webing + newline + // data.labelWeb + data.web
+//  bio + newline + newline +
                carding; // data.labelCard + data.npx
 
 fs.writeFileSync( path.join( __dirname, "bin/output" ), chalk.green( boxen( output, options ) ) )
